041a99ab3757c191eec57fe20a769637f7b320da
Dima A <dimaa@dimaa.ru>
2020-10-17 12:08:14 +0300

    Updates

---
+++ precache-manifest.a101420fcea1f4f1e97fec790a1627cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354b706f6b2d6182b88be15cf8fd50db",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "e6c6bd372eae047f7081",
+    "url": "/spacex/static/css/main.2f699404.chunk.css"
+  },
+  {
+    "revision": "4ba13ddfc0f9015ecec5",
+    "url": "/spacex/static/js/2.c5783ae8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/spacex/static/js/2.c5783ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c6bd372eae047f7081",
+    "url": "/spacex/static/js/main.b6276b55.chunk.js"
+  },
+  {
+    "revision": "38bd822f86f0fa518f7f",
+    "url": "/spacex/static/js/runtime-main.3f7de82f.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spacex/static/media/logo.d5ab31ce.svg"
+  }
+]);
